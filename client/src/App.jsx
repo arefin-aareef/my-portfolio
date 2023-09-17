ee87bf69fee6aa0cc5ebd9762ff9c6911436e7d9
Arefin Aareef <arefin.aareef@gmail.com>
2023-09-17 18:54:11 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+function App() {
+
+  return (
+    <>
+    <Button variant="primary">Primary</Button>{' '}
+
+    </>
+  )
+}
+
+export default App
