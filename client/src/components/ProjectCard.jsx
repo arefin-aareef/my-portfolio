14dd8d1c52d50cbbb9c7bfcb7a585e6fc93a9631
Arefin Aareef <arefin.aareef@gmail.com>
2023-09-18 22:54:00 +0600

    projecs section added

---
+++ client/src/components/ProjectCard.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prop-types */
+import { Col } from "react-bootstrap";
+import { FaExternalLinkSquareAlt } from 'react-icons/fa';
+
+
+const ProjectCard = ({title, description, imgUrl}) => {
+    return (
+        <Col sm={6} md={4}>
+            <div className="proj-imgbx">
+                <img src={imgUrl} alt="Image" />
+                <div className="proj-txtx">
+                    <div>
+                        <h4>{title}</h4>
+                        <span>{description}</span>
+                    </div>
+                    <a type="button" className="btn border-white text-white btn-outline-dark mt-3"><FaExternalLinkSquareAlt className="me-2"></FaExternalLinkSquareAlt>Live</a>
+
+                </div>
+            </div>
+        </Col>
+    );
+};
+
+export default ProjectCard;
