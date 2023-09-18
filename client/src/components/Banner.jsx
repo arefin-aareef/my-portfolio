9571d2781b5a96dcf86d664e85e101a0ffdfda90
Arefin Aareef <arefin.aareef@gmail.com>
2023-09-18 19:19:17 +0600

    skill added

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -20,6 +20,7 @@ export const Banner = () => {
     return () => {
       clearInterval(ticker);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [text]);
 
   const tick = () => {
